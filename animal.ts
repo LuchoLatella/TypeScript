11c89273c7888315bc2707f8fbed45f5920c829a
LuchoLatella <lucho_latella@hotmail.com>
2026-08-20 17:17:39 -0300

    creadas las clases de  Animal(perro, gato, vaca)

--- animal.ts
+++ animal.ts
@@ -1,5 +1,44 @@
-// 1. Interfaz Animal
+// 1. Interfaz de Animal
 interface Animal {
     nombre: string;
     gritar(): string;
 }
+
+// 2. Clases que implementan Animal
+
+class Perro implements Animal {
+    nombre: string;
+
+    constructor(nombre: string) {
+        this.nombre = nombre;
+    }
+
+    gritar(): string {
+        return "Guau";
+    }
+}
+
+class Gato implements Animal {
+    nombre: string;
+
+    constructor(nombre: string) {
+        this.nombre = nombre;
+    }
+
+    gritar(): string {
+        return "Miau";
+    }
+}
+
+class Vaca implements Animal {
+    nombre: string;
+
+    constructor(nombre: string) {
+        this.nombre = nombre;
+    }
+
+    gritar(): string {
+        return "Muuu";
+    }
+}
+
